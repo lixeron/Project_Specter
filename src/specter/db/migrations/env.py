d6f9c3951edacecd52873b3e2e916ea50f104f98
Ethan Tran <etran0155@gmail.com>
2026-05-22 01:16:30 -0500

    async incorporations

--- src/specter/db/migrations/env.py
+++ src/specter/db/migrations/env.py
@@ -20,7 +20,7 @@ def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode."""
     settings = get_settings()
     context.configure(
-        url=settings.database_url,
+        url=settings.async_database_url,
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
@@ -38,7 +38,7 @@ def do_run_migrations(connection) -> None:  # type: ignore[no-untyped-def]
 async def run_async_migrations() -> None:
     """Run migrations in 'online' mode with async engine."""
     settings = get_settings()
-    connectable = create_async_engine(settings.database_url)
+    connectable = create_async_engine(settings.async_database_url)
 
     async with connectable.connect() as connection:
         await connection.run_sync(do_run_migrations)
